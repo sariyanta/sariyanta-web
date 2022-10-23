85da386e9d4b7c240d61740a74e0e5ac1f2013c1
sariyanta <desar@sariyanta.com>
2022-10-23 19:24:13 +0200

    From blog to pages folder

---
+++ pages/blog.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import { getSortedPostsData } from "../src/lib/blog";
+import { BlogArchive } from "../src/components/blog/BlogArchive";
+
+const { heading, subheading } = {
+  heading: "Blog",
+  subheading: "Learning by doing",
+};
+
+export default function Blog({ allPostsData }) {
+  return (
+    <>
+      <Head>
+        <title>{heading}</title>
+      </Head>
+      <BlogArchive posts={allPostsData} />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
