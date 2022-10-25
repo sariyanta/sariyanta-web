eec64ee79ee6d7ec8553e8025a6c2914df420fa2
sariyanta <desar@sariyanta.com>
2022-10-25 21:38:14 +0200

    Start from scratch

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,9 @@ export default function Home() {
 				/>
 			</Head>
 
-			<main className="max-w-4xl mx-auto">
-
+			<main className="flex flex-col max-w-4xl mx-auto">
+				<h1>Hello</h1>
+				<button type="button" role="button" className="inline-block bg-gradient-to-r self-center px-4 py-2 from-black to-red-400 text-white">Sign in</button>
 			</main>
 		</>
 	);
